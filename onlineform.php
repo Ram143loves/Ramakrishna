61ca22608a00ffe11ba0b0de2057c0a41fdd7633
Chandu Veera Rama krishna <30526452+Ram143loves@users.noreply.github.com>
2019-02-14 20:13:18 +0530

    Add files via upload

---
+++ onlineform.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+Participant Name : <?php echo $_POST["name"]; ?><br>
+Qualification    : <?php echo $_POST["year"]; ?> <?php echo $_POST["branch"];?><br>
+Gender           : <?php echo $_POST["gender"]; ?><br>
+College Name     : <?php echo $_POST["collegename"]; ?><br>
+Mobile Number    : <?php echo $_POST["mobilenumber"]; ?><br>
+Email Id         : <?php echo $_POST["email"]; ?><br>
+Participation Event Name : <?php echo $_POST["eventlist"]; ?>
+</body>
+</html>   
